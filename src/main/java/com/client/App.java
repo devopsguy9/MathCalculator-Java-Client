803531c1d4e507e07aaad0604aa4185b0e1753db
devopsguy9 <devopsguy9@gmail.com>
2017-10-29 12:17:55 +0000

    Distribution of Dependency to Client/Other Org

---
+++ src/main/java/com/client/App.java
@@ -0,0 +1,16 @@
+package com.client;
+import com.devopsguy9.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+	System.out.println("Add: "+Calculator.doAddition(10, 50));
+
+    }
+}
